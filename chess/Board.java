2a31e471f02ce7d3c60cb14e23fe99a9d9d42987
Will <williamht123@yahoo.com>
2025-02-27 18:25:37 -0500

    init

--- chess/Board.java
+++ chess/Board.java
@@ -7,47 +7,35 @@ public class Board {
 
     // Method to initialize the chessboard
     public void initializeBoard() {
-        // Reset the board to null
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = null;
-            }
-        }
-
         // Initialize the pieces for both players
         for (int i = 0; i < 8; i++) {
-            board[6][i] = new Piece(PieceType.PAWN, false); // Black pawns
+            // Set up pawns
             board[1][i] = new Piece(PieceType.PAWN, true);  // White pawns
+            board[6][i] = new Piece(PieceType.PAWN, false); // Black pawns
         }
 
-        // Black pieces on rank 0
-        board[7][0] = board[7][7] = new Piece(PieceType.ROOK, false);
-        board[7][1] = board[7][6] = new Piece(PieceType.KNIGHT, false);
-        board[7][2] = board[7][5] = new Piece(PieceType.BISHOP, false);
-        board[7][3] = new Piece(PieceType.QUEEN, false);
-        board[7][4] = new Piece(PieceType.KING, false);
+        // Set up the back ranks (pieces)
+        PieceType[] backRowPieces = {
+            PieceType.ROOK, PieceType.KNIGHT, PieceType.BISHOP, PieceType.QUEEN,
+            PieceType.KING, PieceType.BISHOP, PieceType.KNIGHT, PieceType.ROOK
+        };
 
-        // White pieces on rank 7
-        board[0][0] = board[0][7] = new Piece(PieceType.ROOK, true);
-        board[0][1] = board[0][6] = new Piece(PieceType.KNIGHT, true);
-        board[0][2] = board[0][5] = new Piece(PieceType.BISHOP, true);
-        board[0][3] = new Piece(PieceType.QUEEN, true);
-        board[0][4] = new Piece(PieceType.KING, true);
+        // Black pieces on rank 0 (row 7 in the array)
+        for (int i = 0; i < 8; i++) {
+            board[0][i] = new Piece(backRowPieces[i], true);  // White pieces on rank 8
+            board[7][i] = new Piece(backRowPieces[i], false); // Black pieces on rank 1
+        }
     }
 
     // Method to print the chessboard
     public void printBoard() {
         for (int i = 7; i >= 0; i--) {  // Print from rank 8 to 1
             for (int j = 0; j < 8; j++) {  // Iterate over the files a-h
+                // Print the piece if not null, else print empty space or ##
                 if (board[i][j] != null) {
                     System.out.print(board[i][j].toString() + " ");
                 } else {
-                    // Print empty space for an empty square
-                    if ((i + j) % 2 == 0) {
-                        System.out.print("   ");  // Empty square
-                    } else {
-                        System.out.print("## ");  // Alternatively, you can use "##"
-                    }
+                    System.out.print(((i + j) % 2 == 0) ? "   " : "## "); // Alternating empty spaces
                 }
             }
             System.out.println(i + 1);  // Print the rank number at the end of the row
@@ -56,5 +44,4 @@ public class Board {
         // Print the file letters (a to h)
         System.out.println(" a  b  c  d  e  f  g  h");
     }
-
 }
